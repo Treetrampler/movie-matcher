843034303aec8583ebc08b6882f18580fb040b7d
Treetrampler <treetrampler@gmail.com>
2025-04-23 21:58:22 +1000

    fix: put login checking logic for protected section into layout

--- src/app/(protected)/catalogue/page.tsx
+++ src/app/(protected)/catalogue/page.tsx
@@ -1,13 +1,6 @@
 import { MovieCatalogue } from "@/components/catalogue/movie-catalogue";
-import checkUser from "@/hooks/check-user";
-import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const loggedIn = await checkUser();
-  if (!loggedIn) {
-    redirect("/login");
-  }
-
   return (
     <div className="min-h-screen bg-black">
       <MovieCatalogue />
--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Sidebar } from "@/components/sidebar";
+import checkUser from "@/hooks/check-user";
+import { redirect } from "next/navigation";
 
-export default function ProtectedLayout({
+export default async function ProtectedLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = await checkUser();
+  if (!loggedIn) {
+    redirect("/login");
+  }
   return (
     <div className="flex min-h-screen">
       <Sidebar />
