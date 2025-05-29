b53c7c96118baf1a360198a042eae07e47e2bdf7
Treetrampler <treetrampler@gmail.com>
2025-05-29 18:45:06 +1000

    fix: testapi

--- api/test.py
+++ api/test.py
@@ -1,9 +1,2 @@
-from flask import Flask
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
-
-@app.route("/api/test")
-def handler():
-    return "Hello from Python!"
+def handler(request):
+    return("Hello world", request)
